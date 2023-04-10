d26a56e4a3108bbf6987c9a73ed1f0f3a8dfb34e
gourjoy <goursaha307@gmail.com>
2023-04-11 00:05:38 +0600

    Statistics section complete

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts';
 import SectionTitle from './SectionTitle';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUserAlt } from '@fortawesome/free-solid-svg-icons';
+import { faArrowUp, faUserAlt } from '@fortawesome/free-solid-svg-icons';
 const data = [
 	{
 		name: 'Assignment 1',
@@ -31,34 +31,50 @@ const data = [
 	{
 		name: 'Assignment 7',
 		marks: 60
+	},
+	{
+		name: 'Assignment 8',
+		marks: 60
 	}
 ];
 const Statistics = () => {
 	return (
 		<div>
 			<SectionTitle>Statistics</SectionTitle>
-			<div className='m-auto overflow-scroll lg:overflow-hidden'>
-				<AreaChart
-					width={1000}
-					height={400}
-					data={data}
-					margin={{
-						top: 30,
-						right: 30,
-						left: 30
-					}}>
-					<CartesianGrid strokeDasharray='3 3' />
-					<XAxis dataKey='name' />
-					<YAxis />
-					<Tooltip />
-					<Area type='monotone' dataKey='marks' stroke='#8884d8' fill='#8884d8' />
-				</AreaChart>
-			</div>
-			<div className='px-5 md:px-12 mt-12'>
-				<h3 className='text-2xl font-bold'>
+			<div className='px-5 md:px-12 mt-12 ml-0 md:ml-8'>
+				<h3 className='text-4xl font-bold'>
 					<FontAwesomeIcon className='mr-3' icon={faUserAlt}></FontAwesomeIcon> Gour Saha Joy
 				</h3>
-				<p></p>
+
+				<p className='text-gray-500 ml-16 my-3  text-xl'>Batch 7, Web Development</p>
+				<p className='text-gray-500 text-xl'>
+					<span className='font-bold text-gray-900 '>Average Mark:</span> 59.88
+				</p>
+				<p className='text-gray-500 text-xl'>
+					<span className='font-bold text-gray-900 '>Leader Board Position:</span> 186
+				</p>
+			</div>
+			<div className='m-auto  pb-16'>
+				<div className='overflow-scroll lg:overflow-hidden'>
+					<AreaChart
+						width={1000}
+						height={400}
+						data={data}
+						margin={{
+							top: 30,
+							right: 30,
+							left: 30
+						}}>
+						<CartesianGrid strokeDasharray='3 3' />
+						<XAxis dataKey='name' />
+						<YAxis />
+						<Tooltip />
+						<Area type='monotone' dataKey='marks' stroke='#8884d8' fill='#8884d8' />
+					</AreaChart>
+				</div>
+				<p className='text-2xl font-bold text-center my-6'>
+					<FontAwesomeIcon className='mr-5' icon={faArrowUp} /> Assignment Marks
+				</p>
 			</div>
 		</div>
 	);
