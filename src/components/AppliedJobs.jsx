0f510f1c934d0537546896639cb84624c53a63c3
gourjoy <goursaha307@gmail.com>
2023-04-11 23:02:50 +0600

    filter feature complete

--- src/components/AppliedJobs.jsx
+++ src/components/AppliedJobs.jsx
@@ -1,22 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import { appliedJobs } from '../fakeDB';
 import AppliedJobCard from './Home/JobPosts/AppliedJobCard';
 import SectionTitle from './SectionTitle';
 
 const AppliedJobs = () => {
-	const jobs = [];
+	const foundedAppliedJobs = [];
+	const [jobs, setJobs] = useState(foundedAppliedJobs);
 	const jobPosts = useLoaderData();
 	const appliedJobsInDB = appliedJobs();
+
 	for (const id in appliedJobsInDB) {
-		const foundAppliedJob = jobPosts.find((job) => job.id === id);
-		if (foundAppliedJob) {
-			jobs.push(foundAppliedJob);
+		const findingAppliedJob = jobPosts.find((job) => job.id === id);
+		if (findingAppliedJob) {
+			foundedAppliedJobs.push(findingAppliedJob);
 		}
 	}
 	const handleFilter = (event) => {
-		console.log(event.target.value);
+		const filteredJobs = [];
+		// console.log(event.target.value);
+		for (const job of foundedAppliedJobs) {
+			if (job.remoteOrOnsite === event.target.value) {
+				filteredJobs.push(job);
+			}
+		}
+
+		setJobs(filteredJobs);
 	};
+	console.log(foundedAppliedJobs);
 	return (
 		<div>
 			<SectionTitle>Applied Jobs</SectionTitle>
@@ -28,8 +39,9 @@ const AppliedJobs = () => {
 					<option value='default' disabled>
 						Filter
 					</option>
-					<option value='remote'>Remote</option>
-					<option value='onsite'>Onsite</option>
+					<option value='Onsite'>Show All</option>
+					<option value='Remote'>Remote</option>
+					<option value='Onsite'>Onsite</option>
 				</select>
 				{jobs.map((job) => (
 					<AppliedJobCard key={job.id} job={job} />
